2c35482550ecc3c0a2e64fc90bbfd03253a6046a
Maria Eduarda Nunes Bezerra <depositodanunes@gmail.com>
2025-06-15 21:08:21 -0300

    arrumando "chamar proxima senha"

--- users_sus/manager_urls.py
+++ users_sus/manager_urls.py
@@ -1,9 +1,8 @@
 from django.urls import path
-from . import manager_views 
+from . import manager_views
 
 urlpatterns = [
     path('', manager_views.manager_dashboard, name='manager_dashboard'),
     path('call_next/', manager_views.call_next_code, name='call_next_code'),
-    path('update_status/<uuid:code_id>/', manager_views.update_code_status, name='update_code_status'), 
-    # Adicione outras URLs aqui conforme necessário para funcionalidades do painel
+    path('update_status/<int:code_id>/', manager_views.update_code_status, name='update_code_status'), 
 ]
